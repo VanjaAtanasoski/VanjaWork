af990fa3bae3f8835e6fcb4f9ae2a1698a8d44a4
VanjaAtanasoski <96198385+VanjaAtanasoski@users.noreply.github.com>
2022-04-16 12:39:50 +0200

    Add files via upload

---
+++ TaxiManager9000/Domain/Entities/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxiManager9000.Domain.Enums;
+
+namespace TaxiManager9000.Domain.Entities
+{
+    internal class Admin : User
+    {
+        public Admin(string userName, string password, Role role) : base(userName, password, role)
+        {
+        }
+    }
+}
